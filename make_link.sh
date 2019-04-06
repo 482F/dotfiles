9c05b2aa4e0fbe2a103e81ee37833853a02c560a
482F <482F.git.mail@482f.net>
2019-04-07 00:00:00 +0900

    ln でリンク名にダブルクォーテーションが使えないようなので除去

--- make_link.sh
+++ make_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DFS_DIR="~/git/dotfiles"
 
-ln -s "${DFS_DIR}/.inputrc" "~/.inputrc"
-ln -s "${DFS_DIR}/.screenrc" "~/.screenrc"
-ln -s "${DFS_DIR}/.vimrc" "~/.vimrc"
+ln -s "${DFS_DIR}/.inputrc" ~/.inputrc
+ln -s "${DFS_DIR}/.screenrc" ~/.screenrc
+ln -s "${DFS_DIR}/.vimrc" ~/.vimrc
