a9f0e995dec156b684aa117f5aa2ae9c1b375f96
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-04-09 14:53:57 +0900

    .bashrc_dotfiles を作成

--- make_link.sh
+++ make_link.sh
@@ -4,3 +4,9 @@ DFS_DIR=$(cd $(dirname $0); pwd)
 ln -s "${DFS_DIR}/.inputrc" ~/.inputrc
 ln -s "${DFS_DIR}/.screenrc" ~/.screenrc
 ln -s "${DFS_DIR}/.vimrc" ~/.vimrc
+
+grep -E "^source ${DFS_DIR}/.bashrc_bkfiles$" ~/.bashrc
+
+if [ $? -ne 0 ]; then
+    echo "source ${DFS_DIR}/.bashrc_bkfiles" >> ~/.bashrc
+fi
