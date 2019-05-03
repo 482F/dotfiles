4b33496a807444e62d35744f00e0f67a3be6c1ab
482F <482F.git.mail@482f.net>
2019-05-04 00:00:00 +0900

    第一引数のファイルにあるホストネーム全てに deploy_to_remote.sh を実行するスクリプトを作成

---
+++ deploy_to_remotes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DFS_DIR=$(cd $(dirname $0); pwd)
+declare -a HOSTNAMES=($(cat "${1}"))
+DEPLOY_SH="deploy_to_remote.sh"
+
+for hostname in ${HOSTNAMES[@]}; do
+    bash "${DEPLOY_SH}" "${hostname}"
+done
