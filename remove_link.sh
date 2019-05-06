00518a0a057570f33912e922667dbb5cbfa90a5f
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-05-06 14:26:25 +0900

    dotfiles 関連のものを削除するスクリプトを作成

---
+++ remove_link.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DFS_DIR=$(cd $(dirname $0); pwd)
+declare -a BLACK_LIST=(
+".git"
+".gitignore"
+".bashrc_dotfiles"
+)
+
+cd "${DFS_DIR}"
+
+echo "${DFS_DIR}"
+
+find "${HOME}/" -xtype l | xargs --no-run-if-empty rm
+
+sed -i -e "s/^source ${DFS_DIR}\/\.bashrc_dotfiles$//g" ~/.bashrc
