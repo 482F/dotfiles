ab4d56956f669f65dae4d0fbbc84f64c1bda329a
482F <482F.git.mail@482f.net>
2019-05-27 00:00:00 +0900

    HIST 系の環境変数をコメント化/非コメント化するように

--- remove_link.sh
+++ remove_link.sh
@@ -13,3 +13,6 @@ rm -rf "${DFS_DIR}"
 find "${HOME}/" -xtype l | xargs --no-run-if-empty rm
 
 sed -i -e "s%^source ${DFS_DIR}/.bashrc_dotfiles$%%g" ~/.bashrc
+perl -pi -e 's|^#((export )?HISTSIZE)|$1|' ~/.bashrc
+perl -pi -e 's|^#((export )?HISTFILESIZE)|$1|' ~/.bashrc
+perl -pi -e 's|^#((export )?HISTIGNORE)|$1|' ~/.bashrc
