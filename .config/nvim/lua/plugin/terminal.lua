e911dc4dfb4009a0660aa2b32b02ba35a76dd788
482F <482F.git.mail@482f.net>
2023-09-18 00:00:00 +0900

    ターミナルは buflist から除外するように

--- .config/nvim/lua/plugin/terminal.lua
+++ .config/nvim/lua/plugin/terminal.lua
@@ -1,26 +1,23 @@
 local id = 'terminal-' .. math.random(0, 10000000)
 local i = 0
 
+local function get_name()
+  return id .. '-' .. i
+end
+
 local function open()
   local buffers = vim.tbl_filter(function(v)
     return string.find(v.name, id, 1, true) ~= nil
   end, vim.fn.getbufinfo())
 
   if #buffers <= 0 then
-    vim.cmd.terminal()
-    vim.cmd.file(id .. '-' .. i)
     i = i + 1
-    return
+    vim.cmd.terminal()
+    vim.cmd.file(get_name())
   end
 
-  local name
-  if #buffers == 1 then
-    name = buffers[1].name
-  else
-    return -- TODO: 複数個ターミナルがある時の挙動
-  end
-
-  vim.cmd.edit(name)
+  vim.cmd.edit(get_name())
+  vim.bo[vim.fn.bufnr(get_name())].buflisted = false
 end
 
 local keys = vim.tbl_map(function(v)
@@ -40,6 +37,7 @@ end, {
     func = function()
       vim.cmd.tabnew()
       open()
+      pcall(vim.cmd.bdelete, '#')
     end,
     desc = 'ターミナルを新しいタブで開く',
   },
