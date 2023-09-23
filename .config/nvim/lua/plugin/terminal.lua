af5f05be909fbd59a6c246663af1f77b99eb14e9
482F <482F.git.mail@482f.net>
2023-09-24 00:00:00 +0900

    ターミナル周りの機能追加

--- .config/nvim/lua/plugin/terminal.lua
+++ .config/nvim/lua/plugin/terminal.lua
@@ -1,23 +1,46 @@
 local id = 'terminal-' .. math.random(0, 10000000)
-local i = 0
+local last_i = 0
 
-local function get_name()
-  return id .. '-' .. i
-end
-
-local function open()
-  local buffers = vim.tbl_filter(function(v)
+local function get_terminal_buffers()
+  return vim.tbl_filter(function(v)
     return string.find(v.name, id, 1, true) ~= nil
   end, vim.fn.getbufinfo())
+end
 
-  if #buffers <= 0 then
-    i = i + 1
-    vim.cmd.terminal()
-    vim.cmd.file(get_name())
+local function open(index, force_new)
+  force_new = force_new or false
+
+  local buffers = get_terminal_buffers()
+
+  if index == nil then
+    index = #buffers
   end
 
-  vim.cmd.edit(get_name())
-  vim.bo[vim.fn.bufnr(get_name())].buflisted = false
+  if force_new or (#buffers <= 0) then
+    last_i = last_i + 1
+    index = #buffers + 1
+    vim.cmd.terminal()
+    vim.cmd.file(id .. '-' .. last_i)
+    buffers = get_terminal_buffers()
+  else
+    vim.cmd.edit(buffers[index].name)
+  end
+
+  vim.bo[buffers[index].bufnr].buflisted = false
+end
+
+local function move(delta)
+  local buffers = get_terminal_buffers()
+  local current_bufnr = vim.fn.bufnr()
+  local current_index = 1
+  for j, buffer in pairs(buffers) do
+    if buffer.bufnr == current_bufnr then
+      current_index = j
+      break
+    end
+  end
+
+  open((current_index + delta - 1) % #buffers + 1)
 end
 
 local keys = vim.tbl_map(function(v)
@@ -33,13 +56,49 @@ end, {
     desc = 'ターミナルを開く',
   },
   {
+    suffix = '`',
+    func = function()
+      open(nil, true)
+    end,
+    desc = '新しいターミナルを開く',
+  },
+  {
     suffix = 't',
     func = function()
-      vim.cmd.tabnew()
+      vim.cmd.tabnew('%')
       open()
-      pcall(vim.cmd.bdelete, '#')
     end,
     desc = 'ターミナルを新しいタブで開く',
+  },
+  {
+    suffix = '"',
+    func = function()
+      vim.cmd.new({ args = { '%' }, mods = { split = 'belowright' } })
+      open()
+    end,
+    desc = 'ターミナルを水平に分割して開く',
+  },
+  {
+    suffix = '%',
+    func = function()
+      vim.cmd.vnew({ args = { '%' }, mods = { split = 'belowright' } })
+      open()
+    end,
+    desc = 'ターミナルを垂直に分割して開く',
+  },
+  {
+    suffix = 'k',
+    func = function()
+      move(-1)
+    end,
+    desc = '前のターミナルを開く',
+  },
+  {
+    suffix = 'j',
+    func = function()
+      move(1)
+    end,
+    desc = '次のターミナルを開く',
   },
 })
 
