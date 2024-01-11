915e3bfa01c5de77857635e10266e307ca8fc75f
482F <482F.git.mail@482f.net>
2024-01-12 00:00:00 +0900

    nvim API の補完ができるように

--- .config/nvim/lua/plugin/neodev.lua
+++ .config/nvim/lua/plugin/neodev.lua
@@ -1,4 +1,18 @@
 return {
   'folke/neodev.nvim',
-  ft = 'lua',
+  lazy = false,
+  config = function()
+    require('neodev').setup({
+      library = {
+        enabled = true,
+        runtime = true,
+        types = true,
+        plugins = true,
+      },
+      setup_jsonls = true,
+      override = function(root_dir, options) end,
+      lspconfig = true,
+      pathStrict = true,
+    })
+  end,
 }
