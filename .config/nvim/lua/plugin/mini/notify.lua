495092d32e72b03ffe0d09f275ab303ade270b63
482F <482F.git.mail@482f.net>
2024-01-15 00:00:00 +0900

    mini-notify で LSP の進捗を表示しないように

--- .config/nvim/lua/plugin/mini/notify.lua
+++ .config/nvim/lua/plugin/mini/notify.lua
@@ -1,5 +1,9 @@
 local mini_notify = require('mini.notify')
-mini_notify.setup({})
+mini_notify.setup({
+  lsp_progress = {
+    enable = false,
+  },
+})
 vim.notify = mini_notify.make_notify({
   INFO = { duration = 5000, hl_group = 'DiagnosticHint' },
 })
