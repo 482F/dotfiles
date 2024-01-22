011b7dc029e4e33ef7d696696d706853dee4e73d
482F <482F.git.mail@482f.net>
2024-01-23 00:00:00 +0900

    mini.pairs で `<>` をペアとして登録

--- .config/nvim/lua/plugin/mini/pairs.lua
+++ .config/nvim/lua/plugin/mini/pairs.lua
@@ -1,2 +1,7 @@
 local mini_pairs = require('mini.pairs')
-mini_pairs.setup({})
+mini_pairs.setup({
+    mappings = {
+      ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+      ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+    },
+})
