d6fb27ce3b464130e829ebc8dedc8028eed36427
482F <482F.git.mail@482f.net>
2024-02-13 00:00:00 +0900

    skkeleton の状態を popup で表示するプラグインを追加

--- .config/nvim/lua/plugin/skkeleton.lua
+++ .config/nvim/lua/plugin/skkeleton.lua
@@ -4,6 +4,7 @@ return {
   'vim-skk/denops-skkeleton.vim',
   dependencies = {
     'vim-denops/denops.vim',
+    'NI57721/skkeleton-state-popup',
   },
   config = function()
     vim.api.nvim_create_autocmd({ 'User' }, {
@@ -23,10 +24,24 @@ return {
           showCandidatesCount = 0,
           selectCandidateKeys = '1234567',
           immediatelyOkuriConvert = true,
+          markerHenkan = '',
+          markerHenkanSelect = '',
           kanaTable = 'azik',
         })
 
         vim.fn['skkeleton#register_keymap']('henkan', 'X', false)
+
+        vim.fn['skkeleton_state_popup#config']({
+          labels = {
+            ['input'] = { hira = 'あ', kata = 'ア', hankata = 'ｶﾅ', zenkaku = 'Ａ' },
+            ['input:okurinasi'] = { hira = '▽▽', kata = '▽▽', hankata = '▽▽', abbrev = 'ab' },
+            ['input:okuriari'] = { hira = '▽▽', kata = '▽▽', hankata = '▽▽' },
+            ['henkan'] = { hira = '▼▼', kata = '▼▼', hankata = '▼▼', abbrev = 'ab' },
+            ['latin'] = ' A',
+          },
+          opts = { relative = 'cursor', col = 0, row = 1, anchor = 'NW', style = 'minimal' },
+        })
+        vim.fn['skkeleton_state_popup#enable']()
       end,
     })
   end,
