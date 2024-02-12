b370ef83f744b54fc609e866fdc8b1db3f831324
482F <482F.git.mail@482f.net>
2024-02-13 00:00:00 +0900

    skkeleton の設定のタイミングを読み込み完了時に変更

--- .config/nvim/lua/plugin/skkeleton.lua
+++ .config/nvim/lua/plugin/skkeleton.lua
@@ -6,10 +6,15 @@ return {
     'vim-denops/denops.vim',
   },
   config = function()
-    vim.keymap.set({ 't', 'i', 'c' }, '<C-l>', '<Plug>(skkeleton-toggle)')
-    local skkpath = vim.fn.stdpath('data') .. util.path_delimiter .. 'skk'
-    vim.fn['skkeleton#config']({
-      globalDictionaries = { skkpath .. util.path_delimiter .. 'SKK-JISYO.L' },
+    vim.api.nvim_create_autocmd({ 'User' }, {
+      pattern = { 'DenopsPluginPost:skkeleton' },
+      callback = function()
+        vim.keymap.set({ 't', 'i', 'c' }, '<C-l>', '<Plug>(skkeleton-toggle)')
+        local skkpath = vim.fn.stdpath('data') .. util.path_delimiter .. 'skk'
+        vim.fn['skkeleton#config']({
+          globalDictionaries = { skkpath .. util.path_delimiter .. 'SKK-JISYO.L' },
+        })
+      end,
     })
   end,
 }
