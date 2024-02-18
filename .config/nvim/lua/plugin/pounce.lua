7a85fe931b003a296458fe9ec8609ed202322b27
482F <482F.git.mail@482f.net>
2024-02-19 00:00:00 +0900

    不要な関数の削除

--- .config/nvim/lua/plugin/pounce.lua
+++ .config/nvim/lua/plugin/pounce.lua
@@ -1,30 +1,28 @@
-local util = require('util/init')
-
 local keys = {
   {
     't',
     function()
-      util.lazy_require('pounce').pounce()
+      require('pounce').pounce()
     end,
   },
   {
     'T',
     function()
-      util.lazy_require('pounce').pounce({ do_repeat = true })
+      require('pounce').pounce({ do_repeat = true })
     end,
     mode = 'n',
   },
   {
     't',
     function()
-      util.lazy_require('pounce').pounce({})
+      require('pounce').pounce({})
     end,
     mode = 'x',
   },
   {
     't',
     function()
-      util.lazy_require('pounce').pounce({})
+      require('pounce').pounce({})
     end,
     mode = 'o',
   },
