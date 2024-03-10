bdf17dbaaa31a9fbdbecd9615d0b7b1707f427c0
482F <482F.git.mail@482f.net>
2024-03-11 00:00:00 +0900

    aquarium の色変更

--- .config/nvim/lua/plugin/colorscheme/aquarium.lua
+++ .config/nvim/lua/plugin/colorscheme/aquarium.lua
@@ -3,5 +3,10 @@ return {
   config = function()
     vim.cmd.colorscheme('aquarium')
     vim.cmd.highlight('Comment', 'guifg=#9CA6B9')
+    vim.cmd.highlight('DiagnosticHint', 'guifg=#2e2f6b')
+    vim.cmd.highlight('Pmenu', 'guibg=#e6e6f1')
+    vim.cmd.highlight('StatusLine', 'guibg=#2e2f6b')
+    vim.cmd.highlight('StatusLineNC', 'guifg=#2e2f6b')
+    vim.cmd.highlight('Cursor', 'guifg=#2e2f6b')
   end,
 }
