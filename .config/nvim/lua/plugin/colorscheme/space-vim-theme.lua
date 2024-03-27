0b16a5fa0fcd74becf19f11e72da8646578186a6
482F <482F.git.mail@482f.net>
2024-03-28 00:00:00 +0900

    space-vim-theme の hl 変更

--- .config/nvim/lua/plugin/colorscheme/space-vim-theme.lua
+++ .config/nvim/lua/plugin/colorscheme/space-vim-theme.lua
@@ -2,5 +2,6 @@ return {
   'liuchengxu/space-vim-theme',
   config = function()
     vim.cmd.colorscheme('space_vim_theme')
+    vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal', { bg = '#E5DCCE' })
   end,
 }
