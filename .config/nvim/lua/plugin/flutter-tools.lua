9c559266feda0d1b302c0ee1c56d5c5c6e8d9f34
482F <482F.git.mail@482f.net>
2024-04-28 00:00:00 +0900

    flutter の一部コードアクションをキーマップ定義

--- .config/nvim/lua/plugin/flutter-tools.lua
+++ .config/nvim/lua/plugin/flutter-tools.lua
@@ -1,4 +1,5 @@
 local util = require('util')
+local stream = require('util/stream')
 
 return {
   'akinsho/flutter-tools.nvim',
@@ -6,56 +7,71 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  keys = {
-    {
-      '<leader>pud',
-      function()
-        -- 何故か require('flutter-tools/dev_tools').start() とかを呼んでもうまく行かない
-        vim.cmd.FlutterDevTools()
-        vim.cmd.FlutterCopyProfilerUrl()
-        local url = vim.fn.getreg('*')
-        if url ~= nil then
-          util.open_url(url)
-        end
-      end,
-      desc = 'devtool を開く',
-    },
-    {
-      '<C-F5>',
-      function()
-        require('flutter-tools/devices').list_devices()
-
-        vim.api.nvim_create_autocmd({ 'TextChanged' }, {
-          pattern = { '*__FLUTTER_DEV_LOG__' },
-          once = true,
-          callback = function(event)
-            local console_bufnr = event.buf
-
-            vim.cmd.wincmd('p')
-            require('dapui').open()
-            vim.api.nvim_win_close(
-              vim.fn.win_getid(util.get_winnr_by_bufnr(require('dapui').elements.console.buffer())),
-              false
-            )
-
-            local console_winnr = util.get_winnr_by_bufnr(console_bufnr)
-            vim.fn.win_splitmove(console_winnr, require('plugin/nvim-dap').get_main_winnr(), {
-              rightbelow = true,
-            })
-
-            local console_winid = vim.fn.win_getid(console_winnr)
-            vim.api.nvim_win_set_height(console_winid, 10)
-            vim.bo[console_bufnr].buflisted = false
-
-            vim.keymap.set('n', '<leader>puc', function()
-              vim.fn.win_gotoid(console_winid)
-            end, { desc = 'コンソールに移動' })
+  keys = stream.start({
+        { key = 'ww', title = 'Wrap with widget...' },
+        { key = 'wb', title = 'Wrap with Builder' },
+        { key = 'wc', title = 'Wrap with Column' },
+        { key = 'wr', title = 'Wrap with Row' },
+        { key = 'r',  title = 'Remove this widget' },
+      }).map(function(def)
+        return {
+          '<leader>ll' .. def.key,
+          function()
+            util.exec_code_action(def.title)
           end,
-        })
-      end,
-      desc = '起動',
-    },
-  },
+          desc = def.title,
+        }
+      end)
+      .inserted_all({
+        {
+          '<leader>pud',
+          function()
+            -- 何故か require('flutter-tools/dev_tools').start() とかを呼んでもうまく行かない
+            vim.cmd.FlutterDevTools()
+            vim.cmd.FlutterCopyProfilerUrl()
+            local url = vim.fn.getreg('*')
+            if (url or ''):find('http') == 1 then
+              util.open_url(url)
+            end
+          end,
+          desc = 'devtool を開く',
+        },
+        {
+          '<C-F5>',
+          function()
+            require('flutter-tools/devices').list_devices()
+
+            vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+              pattern = { '*__FLUTTER_DEV_LOG__' },
+              once = true,
+              callback = function(event)
+                local console_bufnr = event.buf
+
+                vim.cmd.wincmd('p')
+                require('dapui').open()
+                vim.api.nvim_win_close(
+                  vim.fn.win_getid(util.get_winnr_by_bufnr(require('dapui').elements.console.buffer())),
+                  false
+                )
+
+                local console_winnr = util.get_winnr_by_bufnr(console_bufnr)
+                vim.fn.win_splitmove(console_winnr, require('plugin/nvim-dap').get_main_winnr(), {
+                  rightbelow = true,
+                })
+
+                local console_winid = vim.fn.win_getid(console_winnr)
+                vim.api.nvim_win_set_height(console_winid, 10)
+                vim.bo[console_bufnr].buflisted = false
+
+                vim.keymap.set('n', '<leader>puc', function()
+                  vim.fn.win_gotoid(console_winid)
+                end, { desc = 'コンソールに移動' })
+              end,
+            })
+          end,
+          desc = '起動',
+        },
+      }).terminate(),
   config = function()
     require('flutter-tools').setup({
       debugger = {
