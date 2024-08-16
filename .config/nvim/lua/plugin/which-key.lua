57fa85b6e186e170330eddf61edd42efbe9e6247
482F <482F.git.mail@482f.net>
2024-08-17 00:00:00 +0900

    deprecated なオプションの除去

--- .config/nvim/lua/plugin/which-key.lua
+++ .config/nvim/lua/plugin/which-key.lua
@@ -9,15 +9,5 @@ return {
     plugins = {
       registers = false,
     },
-    triggers_blacklist = {
-      i = (function()
-        local blackchars = {}
-        local blackstr = '\'"`(){}[]<>'
-        for i = 1, #blackstr, 1 do
-          table.insert(blackchars, blackstr:sub(i, i))
-        end
-        return blackchars
-      end)(),
-    },
   },
 }
