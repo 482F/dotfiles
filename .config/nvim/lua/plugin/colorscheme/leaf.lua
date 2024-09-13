c3907bafa7e55aa654d096d23feed007b5439c34
482F <482F.git.mail@482f.net>
2024-09-14 00:00:00 +0900

    statusline の文字色修正

--- .config/nvim/lua/plugin/colorscheme/leaf.lua
+++ .config/nvim/lua/plugin/colorscheme/leaf.lua
@@ -2,5 +2,9 @@ return {
   'daschw/leaf.nvim',
   config = function()
     vim.cmd.colorscheme('leaf')
+
+    local sl = vim.api.nvim_get_hl(0, { name = 'StatusLineNc' })
+    sl.fg = '#000000'
+    vim.api.nvim_set_hl(0, 'StatusLineNc', sl)
   end,
 }
