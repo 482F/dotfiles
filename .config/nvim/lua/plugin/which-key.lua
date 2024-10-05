2aadf28504a562a2c48088dd6b95e12e6915eee4
482F <482F.git.mail@482f.net>
2024-10-06 00:00:00 +0900

    which-key を <C-c> でキャンセルできるように

--- .config/nvim/lua/plugin/which-key.lua
+++ .config/nvim/lua/plugin/which-key.lua
@@ -1,9 +1,23 @@
+local stream = require('util/stream')
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
+
+    local futil = require('util/func')
+    -- which-key を <C-c> でキャンセルできるように
+    futil.override(vim.fn, 'getcharstr', function(original, ...)
+      local ok, char = pcall(original, ...)
+      if not ok then
+        if char == 'Keyboard interrupt' then
+          require('which-key/util').exit()
+        end
+        error(char, 0)
+      end
+      return char
+    end)
   end,
   opts = {
     preset = 'modern',
