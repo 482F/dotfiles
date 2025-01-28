1275ce5302aea552760d96835101a15a01a0993b
482F <482F.git.mail@482f.net>
2025-01-29 00:00:00 +0900

    mini.pairs で CR をマッピングしないように

--- .config/nvim/lua/plugin/mini/pairs.lua
+++ .config/nvim/lua/plugin/mini/pairs.lua
@@ -1,7 +1,18 @@
 local mini_pairs = require('mini.pairs')
+local stream = require('util/stream')
+
+local mappings = stream.inserted_all(mini_pairs.config.mappings, {
+  ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+  ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+}, 0)
+
+mappings = stream.inserted_all(
+  mappings,
+  stream.map(mappings, function()
+    return { register = { cr = false } }
+  end),
+  0
+)
 mini_pairs.setup({
-    mappings = {
-      ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
-      ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
-    },
+  mappings = mappings,
 })
