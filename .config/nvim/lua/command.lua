cb7455020da8d23b1d476405e698c2ae7b662038
482F <482F.git.mail@482f.net>
2025-02-16 00:00:00 +0900

    コマンド定義方法変更

--- .config/nvim/lua/command.lua
+++ .config/nvim/lua/command.lua
@@ -1,12 +1,22 @@
-vim.api.nvim_create_user_command('Rsib', function()
-  local name = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n'):match('@name%s+([^%s]+)')
-  if name == nil then
-    return
-  end
-  require('plenary/job')
-    :new({
-      command = 'rsib',
-      args = { 'exec', name },
-    })
-    :sync(1000 * 30)
-end, {})
+local stream = require('util/stream')
+
+local commands = {
+  Rsib = {
+    func = function()
+      local name = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n'):match('@name%s+([^%s]+)')
+      if name == nil then
+        return
+      end
+      require('plenary/job')
+        :new({
+          command = 'rsib',
+          args = { 'exec', name },
+        })
+        :sync(1000 * 30)
+    end,
+  },
+}
+
+stream.for_each(commands, function(def, name)
+  vim.api.nvim_create_user_command(name, def.func, {})
+end)
