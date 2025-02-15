abf81dcbe89cc69307bdf60a6df44178e1259dc8
482F <482F.git.mail@482f.net>
2025-02-16 00:00:00 +0900

    totp を実行するコマンドを追加

--- .config/nvim/lua/command.lua
+++ .config/nvim/lua/command.lua
@@ -15,8 +15,19 @@ local commands = {
         :sync(1000 * 30)
     end,
   },
+  Totp = {
+    func = function(opts)
+      local target = opts.fargs[1]
+      local password = vim.fn.inputsecret('password: ')
+      local sysobj = vim.system({ 'totp', target }, { stdin = true })
+      sysobj:write(password .. '\n')
+      local otp = sysobj:wait().stdout:gsub('%s', '')
+      vim.fn.setreg('*', otp)
+    end,
+    opts = { nargs = 1 },
+  },
 }
 
 stream.for_each(commands, function(def, name)
-  vim.api.nvim_create_user_command(name, def.func, {})
+  vim.api.nvim_create_user_command(name, def.func, def.opts or {})
 end)
